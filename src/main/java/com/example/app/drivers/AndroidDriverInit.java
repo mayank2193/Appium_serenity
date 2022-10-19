d3f07c4b5a9c02819feff547ed87e6f4ca208bbc
Mayank Maurya <mayankmaurya2193@gmail.com>
2022-10-19 17:12:33 +0530

    added serenity properties

--- src/main/java/com/example/app/drivers/AndroidDriverInit.java
+++ src/main/java/com/example/app/drivers/AndroidDriverInit.java
@@ -2,25 +2,35 @@ package com.example.app.drivers;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import net.thucydides.core.util.EnvironmentVariables;
+import net.thucydides.core.util.SystemEnvironmentVariables;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Iterator;
 
 public class AndroidDriverInit {
 
     public static AndroidDriver<AndroidElement> driver;
+   
 
     public static void initialize(){
+        EnvironmentVariables environmentVariables = SystemEnvironmentVariables.createEnvironmentVariables();
+
+        String username = environmentVariables.getProperty("lt.user");
+        String accesskey = environmentVariables.getProperty("lt.accessKey");
+
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("platformName", "Android");
-        caps.setCapability("platformVersion", "9");
-        caps.setCapability("deviceName", "Galaxy S10");
-        caps.setCapability("isRealMobile", true);
-        caps.setCapability("app", "lt://");
+        caps.setCapability("platformName", environmentVariables.getProperty("platformName"));
+        caps.setCapability("platformVersion", environmentVariables.getProperty("platformVersion"));
+        caps.setCapability("deviceName", environmentVariables.getProperty("deviceName"));
+        caps.setCapability("isRealMobile", environmentVariables.getProperty("isRealMobile"));
+        caps.setCapability("app", environmentVariables.getProperty("app"));
+
 
         try {
-            driver = new AndroidDriver<AndroidElement>(new URL("https://username:AccessKey@mobile-hub.lambdatest.com/wd/hub"), caps);
+            driver = new AndroidDriver<AndroidElement>(new URL("https://"+username+ ":"+accesskey+"@mobile-hub.lambdatest.com/wd/hub"), caps);
         } catch (MalformedURLException e){
             e.printStackTrace();
         }
